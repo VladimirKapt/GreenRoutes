912fb4b1d2943474b0e0ac599723ac53d93ee6ca
VladimirKapt <vov04ka@bk.ru>
2024-04-19 22:58:12 +0300

    добавил зависимости и основные папки

--- app/src/main/java/com/example/greenroutes/ui/MainActivity.java
+++ app/src/main/java/com/example/greenroutes/ui/MainActivity.java
@@ -1,14 +1,19 @@
-package com.example.greenroutes;
+package com.example.greenroutes.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.greenroutes.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
