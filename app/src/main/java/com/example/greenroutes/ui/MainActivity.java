9a40c03f3cca2c68bf9d44289bc35526f73974eb
Werr2787 <108471157+Werr2787@users.noreply.github.com>
2024-04-20 23:09:45 +0400

    commit

--- app/src/main/java/com/example/greenroutes/ui/MainActivity.java
+++ app/src/main/java/com/example/greenroutes/ui/MainActivity.java
@@ -3,9 +3,16 @@ package com.example.greenroutes.ui;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.greenroutes.R;
 import com.example.greenroutes.databinding.ActivityMainBinding;
+import com.example.greenroutes.ui.domain.Part;
+import com.example.greenroutes.ui.recycler.DemoAdapter;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
@@ -15,7 +22,48 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.rated);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        //binding = ActivityMainBinding.inflate(getLayoutInflater());
+        //setContentView(binding.getRoot());
+        List<Part> items=new LinkedList<>();
+        items.add(new Part("Владимир Дьяченко","1234.0 км","112535"));
+        items.add(new Part("Анис Ячменко","280.5 км","12344"));
+        items.add(new Part("Размик ","288.0 км","1634"));
+        items.add(new Part("Виктор Корнеплод","52.0 км","1375"));
+        items.add(new Part("Олег 1","134.0 км","1573"));
+        items.add(new Part("Олег 2","143.0 км","139"));
+        items.add(new Part("Владимир Дьяченко","1234.0 км","112535"));
+        items.add(new Part("Анис Ячменко","280.5 км","12344"));
+        items.add(new Part("Размик ","288.0 км","1634"));
+        items.add(new Part("Виктор Корнеплод","52.0 км","1375"));
+        items.add(new Part("Олег 1","134.0 км","1573"));
+        items.add(new Part("Олег 2","143.0 км","139"));
+        items.add(new Part("Владимир Дьяченко","1234.0 км","112535"));
+        items.add(new Part("Анис Ячменко","280.5 км","12344"));
+        items.add(new Part("Размик ","288.0 км","1634"));
+        items.add(new Part("Виктор Корнеплод","52.0 км","1375"));
+        items.add(new Part("Олег 1","134.0 км","1573"));
+        items.add(new Part("Олег 2","143.0 км","139"));
+        items.add(new Part("Владимир Дьяченко","1234.0 км","112535"));
+        items.add(new Part("Анис Ячменко","280.5 км","12344"));
+        items.add(new Part("Размик ","288.0 км","1634"));
+        items.add(new Part("Виктор Корнеплод","52.0 км","1375"));
+        items.add(new Part("Олег 1","134.0 км","1573"));
+        items.add(new Part("Олег 2","143.0 км","139"));
+        items.add(new Part("Владимир Дьяченко","1234.0 км","112535"));
+        items.add(new Part("Анис Ячменко","280.5 км","12344"));
+        items.add(new Part("Размик ","288.0 км","1634"));
+        items.add(new Part("Виктор Корнеплод","52.0 км","1375"));
+        items.add(new Part("Олег 1","134.0 км","1573"));
+        items.add(new Part("Олег 2","143.0 км","139"));
+        items.add(new Part("Владимир Дьяченко","1234.0 км","112535"));
+        items.add(new Part("Анис Ячменко","280.5 км","12344"));
+        items.add(new Part("Размик ","288.0 км","1634"));
+        items.add(new Part("Виктор Корнеплод","52.0 км","1375"));
+        items.add(new Part("Олег 1","134.0 км","1573"));
+        items.add(new Part("Олег 2","143.0 км","139"));
+        RecyclerView recyclerView=findViewById(R.id.rectanglerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        DemoAdapter adapter=new DemoAdapter(items);
+        recyclerView.setAdapter(adapter);
     }
 }
